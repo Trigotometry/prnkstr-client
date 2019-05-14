72015f3c301e7f377e408719578c4dfb5cf130f9
Thomas Hexton <tom.hexton@gmail.com>
2019-05-14 12:37:15 +1000

    Updates

---
+++ precache-manifest.51d375796b0116aa6e9d1482742c3e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a686fb747f0d5d2efef7b18143de6f",
+    "url": "/prnkstr-react/index.html"
+  },
+  {
+    "revision": "4540226fa6c0ccebaadf",
+    "url": "/prnkstr-react/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "543b184b506230411e45",
+    "url": "/prnkstr-react/static/js/2.e005614f.chunk.js"
+  },
+  {
+    "revision": "4540226fa6c0ccebaadf",
+    "url": "/prnkstr-react/static/js/main.e817048b.chunk.js"
+  },
+  {
+    "revision": "71e62fe924a677d6573e",
+    "url": "/prnkstr-react/static/js/runtime~main.03ee8686.js"
+  }
+]);
