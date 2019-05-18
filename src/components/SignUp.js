e20e868defad4cf4f549c3c4e958e9adc304229f
Thomas Hexton <tom.hexton@gmail.com>
2019-05-18 15:14:05 +1000

    updating sign in page spacing

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -48,13 +48,13 @@ class SignUp extends Component {
 					<div className="row h-100 d-flex flex-column justify-content-center align-content-center">
 						<div className="col-lg-4 col-md-6">
 							<div className="card">
-								<div className="card-body">
+								<div className="card-body p-5 text-center">
 									<div className="row justify-content-center">
 										<div className="col">
-											<img className="img-fluid py-4" src={process.env.PUBLIC_URL + '/prnkstr_long_logo.svg'} />
+											<img className="img-fluid pb-4" src={process.env.PUBLIC_URL + '/prnkstr_long_logo.svg'} />
 										</div>
 									</div>
-									<div className="row justify-content-center text-center">
+									<div className="row justify-content-center text-center pb-2">
 										<div className="col">
 												<h4>Sign Up</h4>
 										</div>
@@ -76,7 +76,7 @@ class SignUp extends Component {
 												</div>
 											</div>
 										</div>
-										<div className="row justify-content-center mb-4">
+										<div className="row justify-content-center mb-3">
 											<div className="col">
 												<div className="form-group" controlId="formBasicPassword">
 													<label>Confirm Password</label>
